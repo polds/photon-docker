fbbe02d38e155484d44383cb4faeb2ac3723032c
Peter Olds <peter@olds.co>
2025-05-03 16:51:15 -0700

    feat: update entrypoint
    Add the ability to use previously downloaded archives.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,59 @@
 #!/bin/bash
+# This script is used to start the Photon geocoder service in a Docker container.
+
+DATA_DIR="${DATA_DIR:-"/photon/photon_data/elasticsearch"}"
+DOWNLOADS_DIR="${DOWNLOADS_DIR:-}"
+COUNTRY=${COUNTRY:-"all"}
+FILE="photon-db-latest.tar.bz2"
+LINK="http://download1.graphhopper.com/public/${FILE}"
+if [ "$COUNTRY" != "all" ]; then
+    echo "Using country code: $COUNTRY"
+    FILE="photon-db-${COUNTRY}-latest.tar.bz2"
+    LINK="http://download1.graphhopper.com/public/extracts/by-country-code/${COUNTRY}/${FILE}"
+fi
+
+# Check if the downloads directory is set and exists.
+if [ -n "$DOWNLOADS_DIR" ] && [ -d "$DOWNLOADS_DIR" ] && [ ! -d "$DATA_DIR" ]; then
+    # Check if $DOWNLOADS_DIR/$FILE exists.
+    if [ -f "$DOWNLOADS_DIR/$FILE" ]; then
+        echo "Using previously downloaded file $FILE"
+    
+        # Extract the file to $DATA_DIR.
+        echo "Extracting pre-existing $FILE to $DATA_DIR"
+        mkdir -p "$DATA_DIR"
+        tar -xjf "$DOWNLOADS_DIR/$FILE" -C "$DATA_DIR"
+    fi
+fi
 
 
-# Download elasticsearch index
-if [ ! -d "/photon/photon_data/elasticsearch" ]; then
-    echo "Downloading search index"
+# Download elasticsearch index.
+if [ ! -d "$DATA_DIR" ]; then
+    echo "Downloading search index..."
 
     # Let graphhopper know where the traffic is coming from
     USER_AGENT="docker: tonsnoei/photon-geocoder"
-    # If you want to install a specific region only, enable the line below and disable the current 'wget' row.
-    # Take a look at http://download1.graphhopper.com/public/extracts/by-country-code for your country
-    # wget --user-agent="$USER_AGENT" -O - http://download1.graphhopper.com/public/extracts/by-country-code/nl/photon-db-nl-latest.tar.bz2 | bzip2 -cd | tar x
-    wget --user-agent="$USER_AGENT" -O - http://download1.graphhopper.com/public/photon-db-latest.tar.bz2 | bzip2 -cd | tar x
+
+    # Check if DOWNLOADS_DIR is set and exists, if so, download the file there and extract it.
+    # If it doesn't exist, extract it as a stream.
+    if [ -n "$DOWNLOADS_DIR" ] && [ -d "$DOWNLOADS_DIR" ]; then
+        echo "Downloading to $DOWNLOADS_DIR"
+        mkdir -p "$DATA_DIR"
+        wget --user-agent="${USER_AGENT}" -O "$DOWNLOADS_DIR/$FILE" "$LINK"
+        echo "Extracting $FILE to $DATA_DIR"
+        tar -xjf "$DOWNLOADS_DIR/$FILE" -C "$DATA_DIR"
+    else
+        echo "WARNING: DOWNLOADS_DIR not set or does not exist, downloading to $DATA_DIR"
+        echo "Downloading to $DATA_DIR"
+        mkdir -p "$DATA_DIR"
+        wget --user-agent="$USER_AGENT" -O - "$LINK" | bzip2 -cd | tar x
+    fi
 fi
 
 # Start photon if elastic index exists
-if [ -d "/photon/photon_data/elasticsearch" ]; then
+if [ -d "$DATA_DIR" ]; then
     echo "Start photon"
-    java -jar photon.jar $@
+    java -jar photon.jar "$@"
 else
     echo "Could not start photon, the search index could not be found"
+    exit 1
 fi
